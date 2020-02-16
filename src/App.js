e1385e55d544a5e0bbfb4ea8bbf6110e7b922a09
José Augusto Thomas <thomas.joseaugusto@gmail.com>
2020-02-16 20:47:47 -0300

    Testing a change in a feature branch

--- src/App.js
+++ src/App.js
@@ -7,9 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hi there! Bye there!
-        </p>
+        <p>I was changed in a feature branch</p>
         <a
           className="App-link"
           href="https://reactjs.org"
